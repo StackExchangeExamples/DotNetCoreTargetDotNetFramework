2dfca38658cd9d169f0686b433ef6debd067d0a8
JeremyCaney <jeremy.caney@ignia.com>
2020-06-24 16:33:53 -0700

    Establish basic ASP.NET Core 2.2 Project
    This establishes a basic ASP.NET Core 2.2 class library, using Visual Studio's ASP.NET Web Application template, and configures it to instead target .NET Framework 4.8. This helps demonstrate the difference in functionality, as this will be able to access the .NET Framework's WebConfigurationManager class—and, thus, successfully execute BasicClass.GetAppSettings().

---
+++ BasicCore2Website/Controllers/BasicController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BasicCore2Website.Controllers {
+  public class BasicController: Controller {
+    public IActionResult Index(string name = "Stack Overflow") => Content(BasicFrameworkLibrary.BasicClass.SayHello(name));
+    public IActionResult Configuration() => Content(String.Join(", ", BasicFrameworkLibrary.BasicClass.GetAppSettings()));
+  }
+}
