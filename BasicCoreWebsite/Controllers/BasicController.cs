50ab39b6f32a4564feaf8159ff53e6c0df38305a
JeremyCaney <jeremy.caney@ignia.com>
2020-06-24 13:16:50 -0700

    Introduce BasicController
    The BasicController has a single action—Index—which relays to a call of the BasicFrameworkLibrary. In addition, this commit configures the application to map controllers and the default route.

---
+++ BasicCoreWebsite/Controllers/BasicController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicCoreWebsite.Controllers {
+  public class BasicController: Controller {
+    public IActionResult Index(string name = "Stack Overflow") {
+      return Content(BasicFrameworkLibrary.BasicClass.SayHello(name));
+    }
+  }
+}
--- BasicCoreWebsite/Startup.cs
+++ BasicCoreWebsite/Startup.cs
@@ -13,6 +13,7 @@ namespace BasicCoreWebsite {
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services) {
+      services.AddControllers();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,7 @@ namespace BasicCoreWebsite {
       app.UseRouting();
 
       app.UseEndpoints(endpoints => {
+        endpoints.MapDefaultControllerRoute();
         endpoints.MapGet("/", async context => {
           await context.Response.WriteAsync("Hello World!");
         });
