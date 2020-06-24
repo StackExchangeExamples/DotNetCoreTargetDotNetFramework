fa02aacbb487a0863d75584dc81c25a08356bc5e
JeremyCaney <jeremy.caney@ignia.com>
2020-06-24 12:50:16 -0700

    Establish basic .NET Framework Project
    This establishes a basic .NET Framework 4.8 class library, using Visual Studio's .NET Class Library template.

---
+++ BasicFrameworkLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicFrameworkLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicFrameworkLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3bdce232-61dc-478e-8ef1-5b5f8a3bbbc2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
