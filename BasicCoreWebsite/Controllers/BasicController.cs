225cba2762c1f912a184bb812a3a604c7209ceaf
JeremyCaney <jeremy.caney@ignia.com>
2020-06-24 16:31:42 -0700

    Integrate Framework-specific dependency into controller
    Call the new BasicClass.GetAppSettings() method into the BasicController as a new Configuration action. This won't throw any compiler errors—but it also won't be able to run properly, since the WebConfigurationManager doesn't exist in .NET Core.

--- BasicCoreWebsite/Controllers/BasicController.cs
+++ BasicCoreWebsite/Controllers/BasicController.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace BasicCoreWebsite.Controllers {
   public class BasicController: Controller {
-    public IActionResult Index(string name = "Stack Overflow") {
-      return Content(BasicFrameworkLibrary.BasicClass.SayHello(name));
-    }
+    public IActionResult Index(string name = "Stack Overflow") => Content(BasicFrameworkLibrary.BasicClass.SayHello(name));
+    public IActionResult Configuration() => Content(String.Join(", ", BasicFrameworkLibrary.BasicClass.GetAppSettings()));
   }
 }
