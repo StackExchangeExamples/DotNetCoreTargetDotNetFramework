a4e7923cde4a1769c15bce05c7015f250b4f4a35
JeremyCaney <jeremy.caney@ignia.com>
2020-06-24 16:26:33 -0700

    Introduce Framework-specific dependency
    The WebConfigurationManager is a legacy library that is supported in .NET Framework, but which has no analog in .NET Core 3.x. As such, it will be supported on a project that has full access to the .NET Framework, but not on a project built entirely on .NET Core. This new `GetAppSettings()` method simply returns an array of any `<AppSettings />` that are part of the `web.config` file; if there aren't any, it will return an empty array.

--- BasicFrameworkLibrary/BasicClass.cs
+++ BasicFrameworkLibrary/BasicClass.cs
@@ -1,6 +1,9 @@
-﻿namespace BasicFrameworkLibrary {
+﻿using System.Web.Configuration;
+
+namespace BasicFrameworkLibrary {
   public static class BasicClass
-    {
-      public static string SayHello(string name) => $"Hello, {name}";
-    }
+  {
+    public static string SayHello(string name) => $"Hello, {name}";
+    public static string[] GetAppSettings() => WebConfigurationManager.AppSettings.AllKeys;
+  }
 }
